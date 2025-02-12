4d3dfd6e98c6e67f0c8fe43a5a8c4addca018072
Carlo Astudillo <carloasto11@gmail.com>
2025-02-12 22:35:16 +0800

    Add app module with Controller-RESTapi-Post

---
+++ app/src/main/java/com/carloprogram/app/MainApplication.java
@@ -0,0 +1,30 @@
+package com.carloprogram.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.carloprogram")
+@EntityScan(basePackages = "com.carloprogram.model")
+@EnableJpaRepositories(basePackages = "com.carloprogram.repository")
+@RestController
+public class MainApplication {
+
+    @RequestMapping("/")
+    String home(){
+        return "It works. The Server is running";
+    }
+
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
