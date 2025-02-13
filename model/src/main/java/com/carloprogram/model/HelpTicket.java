cb57b8e2c235b5d43cf66890ee965f6271c06a1f
ast2u <carloasto11@gmail.com>
2025-02-13 15:15:24 +0800

    Modify Helpticket model (work in progress)

--- model/src/main/java/com/carloprogram/model/HelpTicket.java
+++ model/src/main/java/com/carloprogram/model/HelpTicket.java
@@ -1,16 +1,33 @@
 package com.carloprogram.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+//@Entity
+//@Table(name = "help_ticket")
 public class HelpTicket {
-    private int ticket_number;
-    private String title;
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ticket_number;
+
+    //@Column(name = "title", nullable = false)
+    private String ticket_title;
+
+    //@Column(name = "body", nullable = false)
     private String body;
+
+
     private String assignee;
+
+    //@Column(name = "status", nullable = false)
     private String status;
     private Date created_date;
     private String created_by;
