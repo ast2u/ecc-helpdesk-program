500edc8ed6b8a2a2957f61e999dd3dfd22956e84
Carlo Astudillo <carloasto11@gmail.com>
2025-02-14 16:39:12 +0800

    Refactor EmployeeMapper on both mapper to employee and DTO, fix mapping employee roles

--- utils/src/main/java/com/carloprogram/mapper/EmployeeMapper.java
+++ utils/src/main/java/com/carloprogram/mapper/EmployeeMapper.java
@@ -1,11 +1,17 @@
 package com.carloprogram.mapper;
 
+import com.carloprogram.dto.EmployeeRoleDto;
 import com.carloprogram.model.Employee;
 import com.carloprogram.dto.EmployeeDto;
+import com.carloprogram.model.enums.EmploymentStatus;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class EmployeeMapper {
 
     public static EmployeeDto mapToEmployeeDto(Employee employee){
+
         return new EmployeeDto(
                 employee.getId(),
                 employee.getFirstName(),
@@ -14,21 +20,22 @@ public class EmployeeMapper {
                 employee.getAddress(),
                 employee.getContactNumber(),
                 employee.getEmploymentStatus(),
-                employee.getEmployeeRole() //fix
+                employee.getEmployeeRoles()
+                        .stream()
+                        .map(EmployeeRoleMapper::mapToEmployeeRoleDto) // Convert role entities to IDs
+                        .collect(Collectors.toSet())
         );
     }
 
     public static Employee mapToEmployee(EmployeeDto employeeDto){
-        return new Employee(
-                employeeDto.getId(),
-                employeeDto.getFirstName(),
-                employeeDto.getLastName(),
-                employeeDto.getAge(),
-                employeeDto.getAddress(),
-                employeeDto.getContactNumber(),
-                employeeDto.getEmploymentStatus(),
-                employeeDto.getEmployeeRoleId() //fix
-
-        );
+        Employee employee = new Employee();
+        employee.setId(employeeDto.getId());
+        employee.setFirstName(employeeDto.getFirstName());
+        employee.setLastName(employeeDto.getLastName());
+        employee.setAge(employeeDto.getAge());
+        employee.setAddress(employeeDto.getAddress());
+        employee.setContactNumber(employeeDto.getContactNumber());
+        employee.setEmploymentStatus(employeeDto.getEmploymentStatus());
+        return employee;
     }
 }
