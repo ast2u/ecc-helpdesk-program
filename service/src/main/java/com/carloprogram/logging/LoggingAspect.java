1de80f261b2470e0f9bcf9e62d3acc3f29a788b9
Carlo Astudillo <carloasto11@gmail.com>
2025-02-20 00:25:04 +0800

    Add Logging for Service, prints the package-method and execution time

---
+++ service/src/main/java/com/carloprogram/logging/LoggingAspect.java
@@ -0,0 +1,25 @@
+package com.carloprogram.logging;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Around("@annotation(LogExecution)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+
+        logger.info("Executed {} in {} ms", joinPoint.getSignature(), executionTime);
+        return result;
+    }
+}
