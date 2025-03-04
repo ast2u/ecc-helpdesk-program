da71d4532b3a4fbee7bd2bb354b710d028831227
ast2u <carloasto11@gmail.com>
2025-03-04 15:47:06 +0800

    Remove unused dependency and refactor on findbyId

--- repo/src/main/java/com/carloprogram/repository/EmployeeRepository.java
+++ repo/src/main/java/com/carloprogram/repository/EmployeeRepository.java
@@ -1,13 +1,10 @@
 package com.carloprogram.repository;
+
 import com.carloprogram.model.Employee;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
--- repo/src/main/java/com/carloprogram/repository/HelpTicketRepository.java
+++ repo/src/main/java/com/carloprogram/repository/HelpTicketRepository.java
@@ -1,10 +1,11 @@
 package com.carloprogram.repository;
 
 import com.carloprogram.model.HelpTicket;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface HelpTicketRepository extends JpaRepository<HelpTicket, Long>, JpaSpecificationExecutor<HelpTicket> {
+    Optional<HelpTicket> findByIdAndDeletedFalse(Long id);
 }
