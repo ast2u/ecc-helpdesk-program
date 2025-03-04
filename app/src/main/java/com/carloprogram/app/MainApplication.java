9645ddfdb4da5551d1d8d29aea70ccbda00dd086
ast2u <carloasto11@gmail.com>
2025-03-04 13:38:33 +0800

    Refactor to fix warning on PageImpl to use PageModel

--- app/src/main/java/com/carloprogram/app/MainApplication.java
+++ app/src/main/java/com/carloprogram/app/MainApplication.java
@@ -5,14 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.carloprogram")
 @EntityScan(basePackages = "com.carloprogram.model")
 @EnableJpaRepositories(basePackages = "com.carloprogram.repository")
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class MainApplication {
 
     public static void main(String[] args)
