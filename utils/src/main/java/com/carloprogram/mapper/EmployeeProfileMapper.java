035f8e6c87c7079930ac64bd3a3f86488101c8c0
Carlo Astudillo <carloasto11@gmail.com>
2025-03-06 00:32:37 +0800

    Refactor mapper to have compute age

--- utils/src/main/java/com/carloprogram/mapper/EmployeeProfileMapper.java
+++ utils/src/main/java/com/carloprogram/mapper/EmployeeProfileMapper.java
@@ -4,9 +4,18 @@ import com.carloprogram.dto.EmployeeProfileDto;
 import com.carloprogram.model.Employee;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 @Mapper(componentModel = "spring")
 public interface EmployeeProfileMapper {
-    @Mapping(target = "age", expression = "java(com.carloprogram.util.employee.DateUtil.computeAge(employee.getBirthDate()))")
+    @Mapping(target = "age", source = "birthDate", qualifiedByName = "computeAge")
     EmployeeProfileDto toProfileDto(Employee employee);
+
+    @Named("computeAge")
+    static int computeAge(LocalDate birthDate) {
+        return (birthDate == null) ? 0 : Period.between(birthDate, LocalDate.now()).getYears();
+    }
 }
