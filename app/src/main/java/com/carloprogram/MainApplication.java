c7ce58573a9dd46d26da9d32dca275edf4cb2c05
Carlo Astudillo <carloasto11@gmail.com>
2025-04-04 14:15:26 +0800

    Move file from app package to root

--- app/src/main/java/com/carloprogram/MainApplication.java
+++ app/src/main/java/com/carloprogram/MainApplication.java
@@ -1,20 +1,13 @@
-package com.carloprogram.app;
+package com.carloprogram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.carloprogram")
-@EntityScan(basePackages = "com.carloprogram.model")
-@EnableJpaRepositories(basePackages = "com.carloprogram.repository")
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 //@EnableCaching
 public class MainApplication {
